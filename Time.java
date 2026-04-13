469f088d609b2ca15928bd7c23b1f04f4b9b784e
Kananelo <kamofeec025@student.wethinkcode.co.za>
2026-04-13 09:23:50 +0200

    Times Table complete

--- Time.java
+++ Time.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Time {
     public static void main(String[] args) {
-        
+         Scanner scanner = new Scanner(System.in);
+         int size  = scanner.nextInt();
+         times(size);
     }
+
+    static void times(int size) {
+        int[][] table = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%4d", table[i][j]);
+            }
+            System.out.println();
+        }
+    };
 }
